8b5e3403e83ae3e4ebdfdd24bb0bf30c31630c64
PDAMBJM\saufi <saufi@pdambandarmasih.com>
2024-01-15 09:34:36 +0800

    update

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MyApp from './Learn_1/MyApp.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MyApp />
+  </React.StrictMode>,
+)
