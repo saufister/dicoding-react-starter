c191286172c2a6d67ad443a25a9109ec0895ca0b
PDAMBJM\saufi <saufi@pdambandarmasih.com>
2024-01-25 09:53:09 +0800

    tambah kode eventhandler + submit + alert

--- src/Learn_3/MyForm.jsx
+++ src/Learn_3/MyForm.jsx
@@ -8,21 +8,62 @@ class MyForm extends React.Component{
             email : 'moh.saufy@gmail.com',
             gender : 'Wanita',
         };
+
+        //binding context ke event handler 
+        this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
+        this.onEmailChangeEventHandler = this.onEmailChangeEventHandler.bind(this);
+        this.onGenderChangeEventHandler = this.onGenderChangeEventHandler.bind(this);
+        this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+    }
+
+    onNameChangeEventHandler(event){
+        //console.log(event.target.value)
+        const method = (prev) => {
+            console.log(prev.name);
+                return {
+                  name: event.target.value
+                };
+        };
+        this.setState(method);
+    }
+    onEmailChangeEventHandler(event){
+        this.setState((prevEmail) => {
+            return {
+                email : event.target.value
+            };
+        });
+    }
+    onGenderChangeEventHandler(event){
+        this.setState((prevGender) =>{
+            return {
+                gender : event.target.value
+            }
+        });
+    }
+    onSubmitEventHandler(event){
+        event.preventDefault();
+        console.log(this.state);
+        const message = `
+        Name: ${this.state.name}
+        Email: ${this.state.email}
+        Gender: ${this.state.gender}
+      `;
+      alert(message);
     }
 
     render(){
         return(
             <div>
                 <h1> Register Form</h1>
-            <form>
+            <form onSubmit={this.onSubmitEventHandler}>
                 <label htmlFor="name">Nama : </label>
-                <input id="name" type ="text" value ={this.state.name}></input>
+                <input id="name" type ="text" value ={this.state.name} onChange={this.onNameChangeEventHandler}></input>
                  <br></br><br></br>
                 <label htmlFor="email"> Email  : </label>
-                <input id="email" type ="text" value = {this.state.email}></input>
+                <input id="email" type ="text" value = {this.state.email} onChange={this.onEmailChangeEventHandler}></input>
                 <br></br><br></br>
                 <label htmlFor="gender"> Gender : </label>
-                <select id="gender" value ={this.state.gender} >
+                <select id="gender" value ={this.state.gender} onChange={this.onGenderChangeEventHandler} >
                     <option value = "Pria"> Pria</option>
                     <option value = "Wanita">Wanita </option>
                 </select>
